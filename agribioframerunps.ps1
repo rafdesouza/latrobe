f53c394fbca637286348fddb8b30e759a1575a1f
rafdesouza <36763322+rafdesouza@users.noreply.github.com>
2021-11-23 17:28:07 +0800

    Update agribioframerunps.ps1

--- agribioframerunps.ps1
+++ agribioframerunps.ps1
@@ -1,1 +1,1 @@
-Invoke-Sqlcmd -InputFile "https://raw.githubusercontent.com/rafdesouza/latrobe/main/agribioframe.sql?token=AIYPNOQA2N2JLQY45CD5R4DBTSVBA" 
+Invoke-Sqlcmd -InputFile "https://raw.githubusercontent.com/rafdesouza/latrobe/main/agribioframe.sql?token=AIYPNOSSZFMGFN4JEVKSC4TBTS2AI" 
